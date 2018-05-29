5e1e9cd0d670cc8e41686e255de1c1d0551f76ee
Imran Paruk <ImranP@dlsys.co.za>
2018-05-30 00:25:39 +0200

    less quick, less dirty

--- src/quick_and_dirty.py
+++ src/quick_and_dirty.py
@@ -22,10 +22,9 @@ path = os.path.normpath(pathy)
 main_pic = Image.open(path+"\\Example Output\\1_in.jpg")
 main_pix = numpy.array(main_pic)
 
-#print(main_pix)
 print(main_pix.shape)
 
-no_tiles = 60
+no_tiles = 20
 xdiv = int(main_pix.shape[0]/no_tiles)
 ydiv = int(main_pix.shape[1]/no_tiles)
 
@@ -44,7 +43,6 @@ main_pic.save(path+"\\Example Output\\1_in_scaled.jpg", "JPEG")
 main_pix = numpy.array(main_pic)
 main_average = numpy.mean(main_pic, axis=(0, 1))
 
-#print(main_pix)
 print(main_pix.shape)
 
 xdiv = int(main_pix.shape[0]/no_tiles)
@@ -53,12 +51,8 @@ ydiv = int(main_pix.shape[1]/no_tiles)
 print(xdiv)
 print(ydiv)
 print(main_pix.shape[0])
-#mypix = pix[0:xdiv, 0:ydiv].copy()
-    
-#print(mypix)
-#print(mypix.shape)
 
-#transformMat(main_pix)
+
 
 
 #%%
@@ -132,7 +126,7 @@ def rgb_to_lab(rgb):
     np_rgb = numpy.array(rgb)
     
     normalized_rgb = np_rgb / 255
-    #print(normalized_rgb)
+
     cie = numpy.dot(matrix, normalized_rgb)
     
     cie[0] = cie[0] /0.950456
@@ -142,69 +136,26 @@ def rgb_to_lab(rgb):
     a = 500*(func(cie[0]) - func(cie[1]))
     b = 200*(func(cie[1]) - func(cie[2]));
     
-    from skimage import io, color
-    lab = color.rgb2lab(rgb)
+    #from skimage import io, color
+    #lab = color.rgb2lab(rgb)
     
-    return lab
+    return [L, a, b]
     #return numpy.array([L,a,b])
 
-def rgb_to_XYZ(rgb):
-    
-    np_rgb = numpy.array(rgb)
-    
-    np_rgb_norm = np_rgb / 255
-    
-    var_R = np_rgb_norm[0]
-    var_G = np_rgb_norm[1]
-    var_B = np_rgb_norm[2]
-    
-    if ( var_R > 0.04045 ):
-        var_R = numpy.power(( ( var_R + 0.055 ) / 1.055 ) , 2.4)
-    else:
-        var_R = var_R / 12.92
-    if ( var_G > 0.04045 ): 
-        var_G = numpy.power(( ( var_G + 0.055 ) / 1.055 ) , 2.4)
-    else:
-        var_G = var_G / 12.92
-    if ( var_B > 0.04045 ):
-        var_B = numpy.power(( ( var_B + 0.055 ) / 1.055 ) , 2.4)
-    else:
-        var_B = var_B / 12.92
-    
-    var_R = var_R * 100
-    var_G = var_G * 100
-    var_B = var_B * 100
-    
-    X = var_R * 0.4124 + var_G * 0.3576 + var_B * 0.1805
-    Y = var_R * 0.2126 + var_G * 0.7152 + var_B * 0.0722
-    Z = var_R * 0.0193 + var_G * 0.1192 + var_B * 0.9505
-    
-    return numpy.array([X, Y, Z])
-
-rgb_to_XYZ([40,0,0])
 #%%
-   
+
+#d(x,y) = sqrt( (Rx-Ry) + (Gx-Gy) + (Bx-By) ).
 def getEuclDistance(x, y):
     return numpy.linalg.norm(x-y)
      
-
-    
-    
-#d(x,y) = sqrt( (Rx-Ry) + (Gx-Gy) + (Bx-By) ).
-
 def myMax(input_dict, cmp):
     
     maxItem = {}
     for key, value in input_dict.items():
         
-       # myvalue = transformMat(value)
-        exld = getEuclDistance(value, cmp)
-        #print(exld)
-        #print(value)
-        #print(cmp)
-        #print("abc: {0}".format(exld))
-        
-        
+        exld = getEuclDistance(value[2], cmp)
+        #print("key: {0} , val: {1}".format(key, exld))
+
         try:
             list(maxItem.values())[0]
 
@@ -212,21 +163,20 @@ def myMax(input_dict, cmp):
             maxItem.update({key:exld})
 
         else:
-            new_exld = getEuclDistance(value, cmp) 
+            new_exld = getEuclDistance(value[2], cmp) 
             if (new_exld < list(maxItem.values())[0] ):
                 maxItem.clear()
                 maxItem.update({key:exld})
-                
-    #print("def: {0}".format(maxItem))
-
+    #print("max {0}".format(maxItem))
     return maxItem
-
 
 
 
 #%%
 
 
+    
+    
     
 
 def transformMat(numpyArr):
@@ -238,7 +188,7 @@ def transformMat(numpyArr):
     for i in range(0, x-1):
         for j in range(0, y-1):
             tempArr = numpyArr[i, j]
-            tempArrTransform = rgb_to_XYZ(tempArr)
+            tempArrTransform = rgb_to_lab(tempArr)
             numpyArr[i, j] = tempArrTransform
 
     return numpyArr         
@@ -259,52 +209,27 @@ def un_transformMat(numpyArr):
             
 #%%
 
-
+print("yo")
 files = []
 for (dirpath, dirnames, filenames) in walk(path + "\\DB"):
     files.extend(filenames)
     break
 
-#print(files)
-image_dict = {}
-file_dict = {}
-file_dict_transform = {}
-
+tile_dict = {}
 
 for file in files:
-    file_pic = Image.open(path+"\\DB\\" + file)
+    tile_pic = Image.open(path+"\\DB\\" + file)
+    tile_np = numpy.array(tile_pic)
+    title_np_ave = numpy.mean(tile_np, axis=(0, 1))
     
     size = (xdiv, ydiv)
-    file_pic = file_pic.resize(size)
-    
-    
-    
-    file_pix = numpy.array(file_pic)
-    #img = Image.fromarray(file_pix, 'RGB')
-    #img.show()
-    #break
-    #transformMat(file_pix)
-    
-    image_dict[file] = file_pix
-    test = transformMat(file_pix.copy())
-    #file_dict_transform[file] = test
-    
-    file_average = numpy.mean(test, axis=(0, 1))
-    
-    file_dict[file] = file_average
+    tile_pic_mini = tile_pic.resize(size)
+    tile_np_mini = numpy.array(tile_pic_mini)
 
-    #file_size = (20, 20)
-    #file_pic.thumbnail(file_size, Image.ANTIALIAS)
+    tile_np_trans = transformMat(tile_np) 
+    tile_np_trans_ave = numpy.mean(tile_np_trans, axis=(0, 1))
     
-    #temp_pix1 = numpy.array(file_pic)
-    
-    #print(temp_pix1.shape)
-    #print(temp_pix2.shape)
-
-#print(image_dict)
-
-transformMat(main_pix)
-
+    tile_dict[file] = [tile_np_mini, title_np_ave, tile_np_trans_ave]
 
 
 
@@ -312,54 +237,22 @@ transformMat(main_pix)
     
 sub_matrix_dict = {}
 
-
+print("here")
 for i in numpy.arange(xdiv-1,main_pix.shape[0],xdiv):
     for j in numpy.arange(ydiv-1,main_pix.shape[1],ydiv):
         int_i = int(i)
         int_j = int(j)
         
         sub_matrix =main_pix[int_i-(xdiv-1):int_i+1, int_j-(ydiv-1):int_j+1].copy()
-        #print(sub_matrix.shape) # = image_dict[(maxKey)]
-        
-
-
-        #transformMat(sub_matrix)
-        #print( "i: {0} j:{1}".format(int_i,int_i-(xdiv-1)))
-        #print(sub_matrix)
         sub_average = numpy.mean(sub_matrix, axis=(0, 1))
         
-        maximum = myMax(file_dict, numpy.array(sub_average))  
+        sub_average_trans = numpy.mean(transformMat(sub_matrix) , axis=(0, 1)) 
+
+        maximum = myMax(tile_dict, sub_average_trans)  
         maxKey = list(maximum.keys())[0]
-        
-        main_pix[int_i-(xdiv-1):int_i+1, int_j-(ydiv-1):int_j+1] = image_dict[(maxKey)]
-        #print(maxKey)
-        #print(image_dict[(maxKey)].shape)
 
-#        maxKey = list(maximum.keys())[0]
-#        
-#        #print(sub_matrix)
-#        transformMat(sub_matrix)
-#        
-#        sub_average = numpy.mean(sub_matrix, axis=(0, 1))
-#        maximum = myMax(file_dict_transform, numpy.array(sub_average))  #compare `int` version of each item
-#        maxKey = list(maximum.keys())[0]
-#        
-#        print(maxKey)
-#        print(image_dict[(maxKey)].shape)
-##        
-#        #temp_average = numpy.mean(sub_matrix, axis=(0, 1))
-#        #sub_matrix_dict[(int_i-xdiv, int_j-ydiv)] = (sub_matrix, temp_average)
-#        
-#        transformMat(sub_matrix)
-#        
-#        
-#        
+        main_pix[int_i-(xdiv-1):int_i+1, int_j-(ydiv-1):int_j+1] = tile_dict[(maxKey)][0]
 
-#        
-#        
-#        #print(image_dict[(maxKey)])
-#        #print(sub_matrix)
-#        #print("#")
     
     
 #%%
@@ -368,4 +261,14 @@ img = Image.fromarray(main_pix, 'RGB')
 img.show()
 
 #print(sub_matrix_dict)
+
+
+
+
+
+
+
+
+
+
 
