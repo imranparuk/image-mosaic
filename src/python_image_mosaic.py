588349b6eb167f016811d373234b4d7754b28270
Imran Paruk <ImranP@dlsys.co.za>
2018-05-30 12:09:55 +0200

    start of final implimentation
    Consolidating the functions from 'quick_and_dirty' into a better looking class.

---
+++ src/python_image_mosaic.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 30 12:03:36 2018
+
+@author: imranparuk
+"""
+
+from PIL import Image
+import numpy
+from skimage import io, color
+
+import os
+from os import listdir
+from os.path import isfile, join
+import sys
+
+
+class image_mosiac():
+    """
+    This class creates image mosaics in python using numpy and PIL (Pillow) 
+    -An input image is used to create a new image from 'tiles' (other images)    
+    """
+    def __init__(self):
+        pass
+    
+    
+if __name__ == "__main__":
